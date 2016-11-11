a9259293d2296549d6db506eef911f090f608fe3
azwickey-pivotal <vania12R>
2016-11-11 14:15:22 -0500

    updating cluster config jobs

--- ci/tasks/enable-pdx.sh
+++ ci/tasks/enable-pdx.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
+wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
+sudo dpkg -i pivotal-gemfire.deb
+
 echo 'Enabling PDX'
 
 gfsh version
 gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
--e "configure11 pdx --read-serialized=true --disk-store=pdx_store --ignore-unread-fields=false --portable-auto-serializable-classes=com.tmo.*"
+-e "configure pdx --read-serialized=true --disk-store=pdx_store --ignore-unread-fields=false --portable-auto-serializable-classes=com.tmo.*"
