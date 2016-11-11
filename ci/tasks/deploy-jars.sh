a7f602a3a0e77366bc3918841f32985ad38cb238
azwickey-pivotal <vania12R>
2016-11-11 16:57:50 -0500

    updating cluster config jobs

--- ci/tasks/deploy-jars.sh
+++ ci/tasks/deploy-jars.sh
@@ -1,2 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash
+wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
+sudo dpkg -i pivotal-gemfire.deb
+
 echo 'Deploying Auth Jars'
+
+gfsh version
+gfsh \
+-e "connect --locator=${LOCATOR_CONNECTION}" \
+-e "deploy --jar=$JAR"
--- ci/tasks/restart-servers.sh
+++ ci/tasks/restart-servers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 echo 'Restarting Gemfire Servers'
 
 #Needed to login
