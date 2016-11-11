30f9f47f24b371f104b3d78892574737a9ff4ea4
azwickey-pivotal <vania12R>
2016-11-11 14:13:32 -0500

    updating cluster config jobs

--- ci/tasks/enable-pdx.sh
+++ ci/tasks/enable-pdx.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -e
 echo 'Enabling PDX'
+
+gfsh version
+gfsh \
+-e "connect --locator=${LOCATOR_CONNECTION}" \
+-e "configure11 pdx --read-serialized=true --disk-store=pdx_store --ignore-unread-fields=false --portable-auto-serializable-classes=com.tmo.*"
