966042dc4d9fe571808313b4476020532d918be7
azwickey-pivotal <vania12R>
2016-11-11 15:36:16 -0500

    updating cluster config jobs

--- ci/tasks/restart-servers.sh
+++ ci/tasks/restart-servers.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 echo 'Restarting Gemfire Servers'
 
+#Needed to login
+#$ export BOSH_CLIENT=ci
+#$ export BOSH_CLIENT_SECRET=ci-password
+
+echo $BOSH_CACERT > bosh-cacert.cer
+ls -l
+
 #BOSH FLOW
-bosh target $BOSH_URL
-bosh login
+bosh --cacert bosh-cacert.cer arget $BOSH_URL
+bosh status
 bosh download manifest
 bosh restart server-group
