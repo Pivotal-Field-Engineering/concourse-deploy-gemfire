435677d4263415c586a68da8617f68ae3032ecb5
azwickey-pivotal <vania12R>
2016-11-11 11:46:32 -0500

    update docker image to azwickey

--- ci/tasks/validate-multi-az-redundancy-map.sh
+++ ci/tasks/validate-multi-az-redundancy-map.sh
@@ -2,8 +2,7 @@
 ls -lha
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
 sudo dpkg -i pivotal-gemfire.deb
-sudo apt-get update
-sudo apt-get -y install default-jre-headless
+
 gfsh version
 gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
