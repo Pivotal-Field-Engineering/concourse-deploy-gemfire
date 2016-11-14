294dc1deaf5c13623f91844e266ef4b4864fbe56
azwickey-pivotal <vania12R>
2016-11-14 08:25:34 -0500

    updating cluster config jobs

--- ci/tasks/enable-pdx.sh
+++ ci/tasks/enable-pdx.sh
@@ -7,5 +7,5 @@ echo 'Enabling PDX'
 gfsh version
 gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
+-e "create disk-store --name=pdx_store --dir=/var/vcap/data/pdx_store" \
 -e "configure pdx --read-serialized=true --disk-store=pdx_store --ignore-unread-fields=false --portable-auto-serializable-classes=com.tmo.*" \
--e "create disk-store --name=pdx_store --dir=pdx_store"
