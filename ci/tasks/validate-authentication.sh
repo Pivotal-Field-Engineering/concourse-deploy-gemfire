787c97bf539a424596178e7e23eb1c5fc4148e71
azwickey-pivotal <vania12R>
2016-12-15 15:17:58 -0500

    corrected keystore generation

--- ci/tasks/validate-authentication.sh
+++ ci/tasks/validate-authentication.sh
@@ -8,7 +8,7 @@ export PATH
 
 ls -lha
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
-sudo dpkg -i pivotal-gemfire.deb
+dpkg -i pivotal-gemfire.deb
 
 gfsh version
 gfsh \
@@ -24,14 +24,14 @@ echo '<!DOCTYPE client-cache PUBLIC' >> gemfire.xml
 echo '"-//GemStone Systems, Inc.//GemFire Declarative Caching 6.5//EN"' >> gemfire.xml
 echo '"http://www.gemstone.com/dtd/cache6_5.dtd">' >> gemfire.xml
 echo '<client-cache>' >> gemfire.xml
-echo "<pool name=\"client\" subscription-enabled=\"false\"><locator host=\"$LOCATOR_IP\" port=\"10334\"/></pool>" >> gemfire.xml
+echo "<pool name=\"client\" subscription-enabled=\"false\"><locator host=\"$LOCATOR_IP\" port=\"$LOCATOR_PORT\"/></pool>" >> gemfire.xml
 echo '<region name="test-region" refid="PROXY"/>' >> gemfire.xml
 echo '</client-cache>' >> gemfire.xml
 cat gemfire.xml
 
 #Test with valid client
 echo "name=SecurityPkcsClient" >> valid-client.properties
-echo "cache-xml-file=gemfire.xml.xml" >> valid-client.properties
+echo "cache-xml-file=gemfire.xml" >> valid-client.properties
 echo "security-client-auth-init=templates.security.PKCSAuthInit.create" >> valid-client.properties
 echo "security-keystorepath=test-client-keys/valid-client-keystore-1.0.0" >> valid-client.properties
 echo "security-alias=valid-client" >> valid-client.properties
