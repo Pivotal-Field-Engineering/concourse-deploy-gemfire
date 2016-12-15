69d24ed91ac2031416e9d46b18acadf593898c28
azwickey-pivotal <vania12R>
2016-12-15 15:00:23 -0500

    corrected keystore generation

--- ci/tasks/validate-authentication.sh
+++ ci/tasks/validate-authentication.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
+set -xe
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+PATH=$JAVA_HOME/bin:$PATH:$HOME/bin
+export JAVA_HOME
+export JRE_HOME
+export PATH
+
 ls -lha
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
 sudo dpkg -i pivotal-gemfire.deb
