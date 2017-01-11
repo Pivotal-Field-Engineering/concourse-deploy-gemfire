e4550ea602f22035a75732ddaae60f41e62321ac
azwickey-pivotal <vania12R>
2017-01-11 12:49:54 -0500

    updated to build authz

--- ci/tasks/deploy-jars.sh
+++ ci/tasks/deploy-jars.sh
@@ -2,9 +2,9 @@
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" $GEMFIRE_CLIENT_URL
 sudo dpkg -i pivotal-gemfire.deb
 
-echo 'Deploying Auth Jars'
+echo 'Deploying Auth Jars: $JAR'
 
 gfsh version
 gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
--e "deploy --jar=security-release/$JAR"
+-e "deploy --jar=gemfire-security/$JAR"
