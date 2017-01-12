e004d02dfb98d090a5f40abda32c54ad4ea373b6
azwickey-pivotal <vania12R>
2017-01-12 12:32:55 -0500

    adding cluster test for authz

--- ci/tasks/deploy-jars.sh
+++ ci/tasks/deploy-jars.sh
@@ -4,7 +4,10 @@ sudo dpkg -i pivotal-gemfire.deb
 
 echo 'Deploying Auth Jars: $JAR'
 
+#Need to always us same name otherwise the GF Classpath isn't updated and new changes don't take affect
+cp gemfire-security/$JAR gemfire-security/authz.jar
+
 gfsh version
 gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
--e "deploy --jar=gemfire-security/$JAR"
+-e "deploy --jar=gemfire-security/authz.jar"
